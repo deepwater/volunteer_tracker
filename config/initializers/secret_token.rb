35690b624e2fb458e8fab75655803e3fc7d3b280
Cody Carnachan <codyc@spektrym.com>
2013-01-24 14:25:15 +1300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VolunteerTracker::Application.config.secret_token = 'd0b9de718a0dad3b2a811083ce795ce109d2f8c975cfaad06f82407f02c503cc64a5610860ccc386ca9e92a632b399dba33c8c543eecad31f94fa8e43576d736'
