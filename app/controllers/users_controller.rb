079d2e41325e6041bd2ad0b2d106124621b3a75d
Cody Carnachan <codyc@spektrym.com>
2013-02-05 14:07:04 +1300

    Integrated Bootstrap theme.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,16 @@ class UsersController < ApplicationController
       format.json { render json: @user }
     end
   end
+
+  # DELETE /user/1
+  # DELETE /user/1.json
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
 end
