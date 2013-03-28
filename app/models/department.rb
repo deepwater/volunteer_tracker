78aeb468fb14ecc39f048a8cd94345a01ebea85d
Cody Carnachan <codycarnachan@gmail.com>
2013-03-28 16:03:50 +1300

    Still fixing. Also changed the relationship of Dep Managers

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,6 @@
 class Department < ActiveRecord::Base
-	has_one :department_manager
-	has_one :lt_department_manager
+	has_many :department_manager
+	has_many :lt_department_manager
 	
 	has_many :department_blocks
 end
