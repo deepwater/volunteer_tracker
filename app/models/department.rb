6460a30d27f0f243d15f420855972ff7ea6b2e91
Cody Carnachan <codycarnachan@gmail.com>
2013-03-28 16:10:47 +1300

    Work goddamit!

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,6 @@
 class Department < ActiveRecord::Base
-	has_many :department_manager
-	has_many :lt_department_manager
+	has_many :department_managers
+	has_many :lt_department_managers
 	
 	has_many :department_blocks
 end
