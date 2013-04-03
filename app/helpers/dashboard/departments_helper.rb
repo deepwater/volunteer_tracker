f6504c2eab20e458791b6619bad41a853cceba8d
Cody Carnachan <codycarnachan@gmail.com>
2013-04-03 15:59:35 +1300

    Fixed the error when editing hours for department. Allowed users to schedule even if 0 hours have been allocated.

--- app/helpers/dashboard/departments_helper.rb
+++ app/helpers/dashboard/departments_helper.rb
@@ -1,6 +1,6 @@
 module Dashboard::DepartmentsHelper
-	def get_estimated_hours(id)
-		@department = Department.find(id)
+	def get_estimated_hours(department_id)
+		@department = Department.find(department_id)
 		total = 0
 		@department.department_blocks.each do |department_block|
 			start_time = department_block.start_time.utc_date
@@ -13,5 +13,29 @@ module Dashboard::DepartmentsHelper
 		end
 
 		return total
+	end
+
+
+	def get_department_usage(estimated_hours, allocated_hours)
+
+		if allocated_hours && estimated_hours == 0
+			return 100
+		elsif allocated_hours == 0
+			return 100
+		else
+			return ( estimated_hours / allocated_hours ) * 100
+		end
+
+	end
+
+	def get_department_usage_class(estimated_hours, allocated_hours)
+
+		if estimated_hours > allocated_hours
+			return "bar-danger"
+		elsif estimated_hours == allocated_hours
+			return "bar-danger"
+		else
+			return ""
+		end
 	end
 end
