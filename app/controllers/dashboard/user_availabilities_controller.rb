2dac848d233b49da0d89d5b904c76a44817f4290
Cody Carnachan <codycarnachan@gmail.com>
2013-04-16 14:10:24 +1200

    Fixed user scheduling bug.

--- app/controllers/dashboard/user_availabilities_controller.rb
+++ app/controllers/dashboard/user_availabilities_controller.rb
@@ -12,7 +12,10 @@ class Dashboard::UserAvailabilitiesController < ApplicationController
   	UserAvailability.where(user_id: @user_id, day_id: params[:data][:day_id]).delete_all
 
   	params[:data][:availableTimes].each do |availableTime|
-  		UserAvailability.create(user_id: @user_id, time: availableTime, day_id: params[:data][:day_id])
+      time = Time.parse(availableTime)
+      formatted_time = time.strftime("%H:%M")
+
+  		UserAvailability.create(user_id: @user_id, time: formatted_time, day_id: params[:data][:day_id])
   	end
   end
 end
