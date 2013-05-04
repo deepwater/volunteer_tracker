e2a61b74551e34e0ae3e812715f7a485b7673fc3
Cody Carnachan <codycarnachan@gmail.com>
2013-05-04 18:05:42 +1200

    Finished prototype for user availabilities

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# start_date = 1374393600
-# end_date = 1375419600
-
-# current = start_date
-
-# interval = 1800
-
-# results = []
-
-# until current > end_date do
-# 	# results << current
-# 	EventTimeslot.create({:utc_date => current})
-
-# 	current += interval
-# end
-
-Day.create(mday: "21", month: "07", year: "2013")
-Day.create(mday: "22", month: "07", year: "2013")
-Day.create(mday: "23", month: "07", year: "2013")
-Day.create(mday: "24", month: "07", year: "2013")
-Day.create(mday: "25", month: "07", year: "2013")
-Day.create(mday: "26", month: "07", year: "2013")
-Day.create(mday: "27", month: "07", year: "2013")
-Day.create(mday: "28", month: "07", year: "2013")
-Day.create(mday: "29", month: "07", year: "2013")
-Day.create(mday: "30", month: "07", year: "2013")
-Day.create(mday: "31", month: "07", year: "2013")
-Day.create(mday: "01", month: "08", year: "2013")
+Day.create(mday: "21", month: "07", year: "2013", day_type: 0)
+Day.create(mday: "22", month: "07", year: "2013", day_type: 0)
+Day.create(mday: "23", month: "07", year: "2013", day_type: 0)
+Day.create(mday: "24", month: "07", year: "2013", day_type: 0)
+Day.create(mday: "25", month: "07", year: "2013", day_type: 0)
+Day.create(mday: "26", month: "07", year: "2013", day_type: 1)
+Day.create(mday: "27", month: "07", year: "2013", day_type: 1)
+Day.create(mday: "28", month: "07", year: "2013", day_type: 1)
+Day.create(mday: "29", month: "07", year: "2013", day_type: 2)
+Day.create(mday: "30", month: "07", year: "2013", day_type: 2)
+Day.create(mday: "31", month: "07", year: "2013", day_type: 2)
+Day.create(mday: "01", month: "08", year: "2013", day_type: 2)
