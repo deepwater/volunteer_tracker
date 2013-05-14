a94f977265bff1861486633cda037a7adea9a268
Cody Carnachan <codycarnachan@gmail.com>
2013-05-15 10:06:38 +1200

    First iteration of user scheduling

--- app/models/department_block.rb
+++ app/models/department_block.rb
@@ -19,62 +19,64 @@ class DepartmentBlock < ActiveRecord::Base
     return t.strftime("%l.%M%P %A %-d %B")
   end
 
-	def get_perfect_fit_users
+  def overlaps(other)
+    dep_block_start = Time.parse("#{self.start_time} #{self.day.mday}/#{self.day.month}/#{self.day.year}")
+    dep_block_end = Time.parse("#{self.end_time} #{self.day.mday}/#{self.day.month}/#{self.day.year}")
+    
+    other_dep_block_start = Time.parse("#{other.start_time} #{self.day.mday}/#{self.day.month}/#{self.day.year}")
+    other_dep_block_end = Time.parse("#{other.end_time} #{self.day.mday}/#{self.day.month}/#{self.day.year}")
+
+    (dep_block_start - other_dep_block_end) * (other_dep_block_start - dep_block_end) >= 0
+  end
+
+  def duration
+    start_time = Time.parse(self.start_time)
+    end_time = Time.parse(self.end_time)
+    
+    end_time - start_time
+  end
+
+	def get_user_availabilities
   		# Stores eligible and available users
-  		users = []
+  		user_availabilities = []
 
   		# Gets a list of users who have a time availability at the start of the block
       @list_of_availabilities = self.day.user_availabilities
 
+      # Get duration of department block
+      start_time = Time.parse(self.start_time)
+      end_time = Time.parse(self.end_time)
+      duration = self.duration
+
+      availabilities = []
+
       @list_of_availabilities.each do |user_availability|
-        start_time = Time.parse(self.start_time)
-        end_time = Time.parse(self.end_time)
 
-        block_time_range = Time.parse(self.start_time).. Time.parse(self.end_time)
+        if self.overlaps(user_availability)
+          availabilities << user_availability
+        end
 
-        availabilityRange = Time.parse(user_availability.start_time).. Time.parse(user_availability.end_time)
-
-        start_check = UserAvailability.where("(TIMEDIFF(start_time, ?) * TIMEDIFF(?, end_time)) >= 0", user_availability.end_time, user_availability.start_time)
-
-        logger.debug "BLAH #{start_check.all}"
       end
-  		# @users = User.joins(:user_availabilities).where(user_availabilities: {time: self.start_time, day_id: self.day.id}).all
 
-  		# Find length of time 
-  		# start_time = Time.parse(self.start_time)
-  		# end_time = Time.parse(self.end_time)
-  		# duration = (end_time - start_time) / 60
-  		# numberOfUserBlocks = duration / 15
+      availabilities.each do |user_availability|
+        user = user_availability.user
 
-    #   logger.debug "#{@users}"
-  		# # Loop through the set of users
-  		# @users.each do |user|
-  		# 	# Check if user is eligible for each of the EventTimeslots
-  		# 	is_eligible = false
-  		# 	is_available = false
+        eligibility_errors = 0
 
-  		# 	availability_errors = 0
+        user.user_schedules.each do |user_schedule|
+          user_schedule_block = user_schedule.department_block
+          duration = user_schedule_block.duration
 
-  		# 	num_availabilities = user.user_availabilities.where(time: [self.start_time...self.end_time]).length
+          overlap = user_schedule_block.overlap(user_availability)
+          overlap_percentage = (overlap / duration) * 100
 
-  		# 	is_eligible = true if num_availabilities == numberOfUserBlocks
-    #     logger.debug "num_availabilities: #{num_availabilities} numberOfUserBlocks: #{numberOfUserBlocks}"
+          if overlap_percentage > 50
+            eligibility_errors += 1
+          end
+        end
 
-  		# 	# If they passed the eligibility check
-  		# 	if is_eligible
-
-  		# 		availability_errors = 0;
-  		# 		# Loop through user schedules
-
- 			# 	availability_errors += 1 if user.department_blocks.where(start_time: [self.start_time...self.end_time]).length > 0
-  		# 		availability_errors += 1 if user.department_blocks.where(end_time: [self.start_time...self.end_time]).length > 0
-   	# 		end
-
-  		# 	is_available = true if availability_errors == 0 && is_eligible == true
-  			
-  		# 	perfect_users << user if is_eligible && is_available
-  		# end
-
-  		users
+        user_availabilities << user_availability if eligibility_errors == 0 
+      end
+ 
   	end
 end
