18f9ad8d565b338d5281c6fc66a9b8ea99f9a406
Cody Carnachan <codycarnachan@gmail.com>
2013-05-15 13:01:51 +1200

    Scheduling problem where users scheduled on one day would be unavailable for all others should be fixed

--- app/models/user_availability.rb
+++ app/models/user_availability.rb
@@ -1,4 +1,4 @@
 class UserAvailability < ActiveRecord::Base
 	belongs_to :user
-	belongs_to :day, primary_key: 'day_id', foreign_key: 'id'
+	has_one :day, primary_key: 'day_id', foreign_key: 'id'
 end
