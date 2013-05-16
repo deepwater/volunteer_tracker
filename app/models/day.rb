b5d5f229f5f06120a347b31d7514a46bcf947bf6
Cody Carnachan <codycarnachan@gmail.com>
2013-05-16 16:12:50 +1200

    Added date to Department Block Page Fixes #62. Stopped user from being able to midfy time availability when scheduled - Fixes #54. Added 4 hour recommended shift time message to User Availability entry page - Fixes #63

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,7 +2,22 @@ class Day < ActiveRecord::Base
 
 	has_many :user_availabilities
 	has_many :department_blocks
-	has_many :user_schedules, :through => :department_blocks
+	has_many :user_schedules, primary_key: "id", foreign_key: "department_block.day_id"
+
+	# scope :user_schedules, Day.joins(:department_blocks => :model3).where('model3.label' => 'label')
+
+	def user_scheduled?(user)
+		users = []
+
+		self.department_blocks.each do |department_block|
+			department_block.users.each do |user|
+				users << user
+			end
+		end
+		users.include?(user)
+
+	end
+
 
 	def day_name
    		t = Time.new(self.year,self.month,self.mday, 0, 0, 0)
