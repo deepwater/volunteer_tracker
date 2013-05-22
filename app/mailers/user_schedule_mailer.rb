110e8acdaf1abdc286b1147a33a5843dbb53e969
Cody Carnachan <codycarnachan@gmail.com>
2013-05-23 10:54:23 +1200

    Unschedule Email should now be sent. Completes #70

--- app/mailers/user_schedule_mailer.rb
+++ app/mailers/user_schedule_mailer.rb
@@ -7,4 +7,12 @@ class UserScheduleMailer < ActionMailer::Base
 
     mail(to: @user.email, cc: @user.secondary_email.to_s == '' ? nil : @user.secondary_email, from: "noreply@volunteer.gilroygarlicfestival.com", subject: "You have been scheduled!")
   end
+
+  def unschedule_email(user_schedule)
+  	@user = user_schedule.user
+  	@department_block = user_schedule.department_block
+  	@department = @department_block.department
+
+    mail(to: @user.email, cc: @user.secondary_email.to_s == '' ? nil : @user.secondary_email, from: "noreply@volunteer.gilroygarlicfestival.com", subject: "You have been unscheduled!")
+  end
 end
--- app/models/user_schedule.rb
+++ app/models/user_schedule.rb
@@ -4,9 +4,14 @@ class UserSchedule < ActiveRecord::Base
 	belongs_to :department_block
 
 	after_create :deliver_email
+	after_destroy :unschedule_email
 
 	def deliver_email
 		UserScheduleMailer.schedule_email(self).deliver
 	end
 
+	def unschedule_email
+		UserScheduleMailer.unschedule_email(self).deliver
+	end
+	
 end
