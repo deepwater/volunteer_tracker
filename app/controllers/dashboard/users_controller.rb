cd5e2e2b1bd408fa24fb14b214c88cace3d2a3e5
Cody Carnachan <codycarnachan@gmail.com>
2013-05-31 11:24:51 +1200

    Promoting of Users from Department Edit page is now complete. Forgot to add new file. Refs #67. Refs #68.

---
+++ app/controllers/dashboard/users_controller.rb
@@ -0,0 +1,18 @@
+class Dashboard::UsersController < DashboardController
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to :back, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
