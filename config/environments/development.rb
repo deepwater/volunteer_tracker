a0f2bcb735ab93889003dc8a976c54efb8c10f08
Cody Carnachan <codycarnachan@gmail.com>
2013-06-28 17:34:38 +1200

    Attempted fix for user scheduling buginess

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,10 @@ VolunteerTracker::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.action_mailer.perform_deliveries = false
+
+  # UNICORN LOGGING
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(
+    ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'DEBUG'
+  )
 end
