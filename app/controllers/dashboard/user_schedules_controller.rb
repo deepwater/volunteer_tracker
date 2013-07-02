139b1cc5755a9d7bc2438400455a04fd700470a2
Cody Carnachan <codycarnachan@gmail.com>
2013-07-02 13:47:09 +1200

    Added in some niceness to user scheduling and unscheduling

--- app/controllers/dashboard/user_schedules_controller.rb
+++ app/controllers/dashboard/user_schedules_controller.rb
@@ -59,7 +59,7 @@ class Dashboard::UserSchedulesController < ApplicationController
     @user_schedule.destroy
 
     respond_to do |format|
-      format.html { redirect_to [:dashboard, :user_schedules] }
+      format.html { redirect_to :back }
       # format.html { redirect_to [:dashboard, @user_schedule.department_block] }
       format.json { head :no_content }
     end
