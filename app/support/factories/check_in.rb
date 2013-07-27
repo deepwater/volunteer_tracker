9ec5c13fcb93d0425423d478211bb712ba24eb0a
chris cooning <chriscooning@gmail.com>
2013-07-26 19:32:44 -0700

    get cray
    will this work?

--- app/support/factories/check_in.rb
+++ app/support/factories/check_in.rb
@@ -65,7 +65,7 @@ class Factories::CheckIn
 
   def validate_check_in_date(entity)
     if entity.user_schedule && day = entity.user_schedule.try(:department_block).try(:day)
-      entity.errors.add(:created_at, :not_assigned) if day.to_date != Date.today
+      entity.errors.add(:created_at, :not_assigned) if day.to_date != (Date.today + 1)
     end
   end
 
