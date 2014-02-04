ee1094539b224e814ef4f479c9bc560124aa641a
makoviy <artem.gooscha@gmail.com>
2014-02-04 10:42:31 +0200

    AG Setup mailgun

--- config/configatron/staging.rb
+++ config/configatron/staging.rb
@@ -1,1 +1,4 @@
 configatron.host = 'volunteer.23stages.com'
+configatron.smtp.domain = "volunteer.gilroygarlicfestival.com"
+configatron.smtp.username = "postmaster@volunteer.gilroygarlicfestival.com"
+configatron.smtp.password = "9mqiq20nts78"
