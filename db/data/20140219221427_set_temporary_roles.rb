366a7090475d6a5df306421f96b76c82c546a92f
makoviy <artem.gooscha@gmail.com>
2014-02-20 01:29:53 +0200

    AG Full list of temporary admins

--- db/data/20140219221427_set_temporary_roles.rb
+++ db/data/20140219221427_set_temporary_roles.rb
@@ -1,6 +1,6 @@
 class SetTemporaryRoles < ActiveRecord::Migration
   def self.up
-    users = User.where(email: %w[brianbowe@verizon.net jessica@omgmediagroup.com brianbowe@verizon.net] )
+    users = User.where(email: %w[bwj@bjenkins.com craig_miner@ajg.com joann@gilroygarlicfestival.com deanna.franklin@rabobank.com brianbowe@verizon.net jessica@omgmediagroup.com] )
     users.each do |user|
       user.add_role(:super_admin)
     end
