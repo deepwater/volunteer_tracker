e1f519f98cfa5b5a6e61a44726c0a899208b603d
makoviy <artem.gooscha@gmail.com>
2014-02-20 01:04:10 +0200

    AG Fix for username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true
   validates :username, presence: true, unless: :password_required?
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, unless: :password_required?
   validates :email, presence: true, unless: :subaccount?
   validates :password, presence: true, if: :password_required_on_update?
   validates_confirmation_of :password, if: :password_required?
--- db/data/20140219221427_set_temporary_roles.rb
+++ db/data/20140219221427_set_temporary_roles.rb
@@ -1,6 +1,6 @@
 class SetTemporaryRoles < ActiveRecord::Migration
   def self.up
-    users = User.where(email: %w[jessica@omgmediagroup.com brianbowe@verizon.net] )
+    users = User.where(email: %w[brianbowe@verizon.net jessica@omgmediagroup.com brianbowe@verizon.net] )
     users.each do |user|
       user.add_role(:super_admin)
     end
