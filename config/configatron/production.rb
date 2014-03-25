a9bcec8c42c410b51e4f5ff026be07765cc9c6e8
makoviy <artem.gooscha@gmail.com>
2014-03-25 21:02:23 +0200

    Setting up mailer for production

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -1,1 +1,4 @@
 configatron.host = 'volunteer.gilroygarlicfestival.com'
+configatron.smtp.domain = "volunteer.gilroygarlicfestival.com"
+configatron.smtp.username = "postmaster@volunteer.gilroygarlicfestival.com"
+configatron.smtp.password = "9mqiq20nts78"
