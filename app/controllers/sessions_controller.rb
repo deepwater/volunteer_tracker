7fb37e31a82885c6820ec3eba8c481898550a078
makoviy <artem.gooscha@gmail.com>
2014-04-21 21:54:30 +0400

    Not allow subaccounts login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < Devise::SessionsController
 
   def create
-    if params[:on_behalf].present? and user = User.find(params[:on_behalf])
-      sign_in(:user, user)
-      respond_with user, location: after_sign_in_path_for(user)
+    if params[:on_behalf].present? and resource = User.find(params[:on_behalf])
+      sign_in(:user, resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    elsif resource.subaccount?
+      sign_in(:user, resource.master)
+      respond_with resource, location: after_sign_in_path_for(resource)
     else
       super
     end
   end
 
   protected
-  
+
   def after_sign_in_path_for(resource)
     if resource.username.present?
       resource.sign_in_count <= 1 ? '/dashboard/user_availabilities' : root_path
