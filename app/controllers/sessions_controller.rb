75e2cacc0f8e931a25ccf71942da835c0374cdfc
makoviy <artem.gooscha@gmail.com>
2014-05-12 09:08:42 +0400

    Fixed path after sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,15 +13,4 @@ class SessionsController < Devise::SessionsController
     end
   end
 
-  protected
-
-  def after_sign_in_path_for(resource)
-    if resource.username.present?
-      resource.sign_in_count <= 1 ? '/dashboard/user_availabilities' : root_path
-    else
-      flash[:notice] = 'Please enter your username'
-      edit_user_registration_path
-    end
-  end
-
 end
