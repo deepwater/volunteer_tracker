836736a1e85fbca2bd210e7a8bc46990b94e7ee5
makoviy <artem.gooscha@gmail.com>
2014-05-15 20:32:39 +0400

    Fixed Departments List progress bars disappeared on the Admin Panel bug

--- app/helpers/dashboard/departments_helper.rb
+++ app/helpers/dashboard/departments_helper.rb
@@ -1,45 +1,39 @@
 module Dashboard::DepartmentsHelper
-	def get_estimated_hours(department_id)
-		@department = Department.find(department_id)
-		total = 0
-		@department.department_blocks.each do |department_block|
-			start_time = department_block.start_time.utc_date
-			end_time = department_block.end_time.utc_date
+  def get_estimated_hours(department_id)
+    @department = Department.find(department_id)
+    total = 0
+    @department.department_blocks.each do |department_block|
+      start_time = department_block.start_time.utc_date
+      end_time = department_block.end_time.utc_date
 
-			duration_in_minutes = (end_time.to_f - start_time) / 60 
-			duration_in_hours = duration_in_minutes / 60
+      duration_in_minutes = (end_time.to_f - start_time) / 60 
+      duration_in_hours = duration_in_minutes / 60
 
-			if department_block.suggested_number_of_workers
-				total += department_block.suggested_number_of_workers * duration_in_hours
-			end
-		end
+      if department_block.suggested_number_of_workers
+        total += department_block.suggested_number_of_workers * duration_in_hours
+      end
+    end
 
-		return total
-	end
+    return total
+  end
 
 
-	def get_department_usage(estimated_hours, allocated_hours)
-		#logger.debug "EST: #{estimated_hours}"
-		#logger.debug "ALL: #{allocated_hours}"
+  def get_department_usage(estimated_hours, allocated_hours)
 
-		if allocated_hours == 0 && estimated_hours == 0
-			return 100
-		elsif allocated_hours == 0
-			return 100
-		else
-			return ( estimated_hours.to_f / allocated_hours.to_f ) * 100
-		end
+    if allocated_hours == 0
+      return 100
+    else
+      return ( estimated_hours.to_f / allocated_hours.to_f ) * 100
+    end
 
-	end
+  end
 
-	def get_department_usage_class(estimated_hours, allocated_hours)
+  def get_department_usage_class(estimated_hours, allocated_hours)
 
-		if estimated_hours.to_f > allocated_hours.to_f
-			return "bar-danger"
-		elsif estimated_hours.to_f == allocated_hours.to_f
-			return "bar-danger"
-		else
-			return ""
-		end
-	end
+    if estimated_hours.to_f >= allocated_hours.to_f
+      return "progress-bar-danger"
+    else
+      return ""
+    end
+  end
 end
