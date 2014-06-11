fa7bef6ba1c6705d29a014c713243041bfcd3ae6
makoviy <artem.gooscha@gmail.com>
2014-06-11 21:43:12 +0400

    Some fixes on dashboard page

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ActiveRecord::Base
-  ROLES = %w(volunteer volunteer_assistant volunteer_manager department_assistant department_manager
+  ROLES = %w(volunteer volunteer_manager department_assistant department_manager
             event_admin org_admin super_admin)
 
   has_and_belongs_to_many :users, :join_table => :users_roles
