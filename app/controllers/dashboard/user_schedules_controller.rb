455015f834d94648f2ad99b6b3b885de24facaf3
makoviy <artem.gooscha@gmail.com>
2014-07-15 11:11:19 +0400

    [#277] Fixed - charity re-assignment returns error

--- app/controllers/dashboard/user_schedules_controller.rb
+++ app/controllers/dashboard/user_schedules_controller.rb
@@ -21,11 +21,11 @@ class Dashboard::UserSchedulesController < ApplicationController
 
   def create
     @user_schedule = UserSchedule.new(params[:user_schedule])
-    
+
     respond_to do |format|
       if @user_schedule.save
         format.json { render json: { template: render_to_string("dashboard/user_schedules/show.json")} }
-        format.html do 
+        format.html do
           redirect_to dashboard_department_block_path(@user_schedule.department_block, params[:scope]),
             notice: 'User schedule was successfully created.'
         end
@@ -41,15 +41,15 @@ class Dashboard::UserSchedulesController < ApplicationController
 
     respond_to do |format|
       if @user_schedule.update_attributes(params[:user_schedule])
-        format.html { redirect_to [:dashboard,@user_schedule], notice: 'User Schedule was successfully updated.' }
+        format.html { redirect_to [:dashboard, @user_schedule], notice: 'User Schedule was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
+        format.html { redirect_to :back, alert: @user_schedule.errors.full_messages.join(', ') }
         format.json { render json: @user_schedule.errors, status: :unprocessable_entity }
       end
     end
   end
-  
+
   def destroy
     @user_schedule = UserSchedule.find(params[:id])
     @user_schedule.destroy
@@ -60,7 +60,7 @@ class Dashboard::UserSchedulesController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
+
   def dymo
     @user_schedule = UserSchedule.find(params[:id])
   end
