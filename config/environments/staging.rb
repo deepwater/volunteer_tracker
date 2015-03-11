bdc05efbe5a3d9e5ec417b3bb2e2199af422a66b
Artem Gooscha <artem.gooscha@gmail.com>
2015-03-11 20:39:20 +0200

    Fix SMTP mailtrap error

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -27,8 +27,8 @@ VolunteerTracker::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      configatron.host,
-    password:       configatron.host,
+    user_name:      configatron.user_name,
+    password:       configatron.password,
     address:        'mailtrap.io',
     domain:         'mailtrap.io',
     port:           '2525',
