eff568c10fd3de35b82b29bb61c21bec11623183
Artem Gooscha <artem.gooscha@gmail.com>
2015-03-11 18:03:43 +0200

    Fix bug with login DepartmentManager

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,8 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    if %w(volunteer volunteer_manager department_assistant super_admin).include?(current_user.role)
-      @day = Day.first
-      @user_schedules = current_user.user_schedules.includes(department_block: [:day, :department]).limit(5)
-      @past_assignments = current_user.check_ins.where(status: '2').includes(user_schedule: {department_block: [:day, :department]}).order("check_out_time DESC").limit(5).decorate
-    end
+    @day = Day.first
+    @user_schedules = current_user.user_schedules.includes(department_block: [:day, :department]).limit(5)
+    @past_assignments = current_user.check_ins.where(status: '2').includes(user_schedule: {department_block: [:day, :department]}).order("check_out_time DESC").limit(5).decorate
   end
 end
