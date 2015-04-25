e11a0b6de82329bba3bc2feb7b40fc6dd0ee4201
Artem Gooscha <artem.gooscha@gmail.com>
2015-04-25 09:09:15 +0300

    Amended README description

--- config/deploy/production2.rb
+++ config/deploy/production2.rb
@@ -12,7 +12,7 @@ role :db,  dns_name, primary: true           # This is where Rails migrations wi
 set :deploy_to, "/data/#{application}"
 
 set :rails_env, 'production'
-set :branch, 'sausalito'
+set :branch, 'saf_tracker_production'
 set :use_sudo, false
 
 set :user, 'ninja'
--- config/deploy/staging2.rb
+++ config/deploy/staging2.rb
@@ -14,7 +14,7 @@ role :db,  dns_name, primary: true           # This is where Rails migrations wi
 set :deploy_to, "/data/#{application}"
 
 set :rails_env, 'staging'
-set :branch, 'master'
+set :branch, 'saf_tracker'
 set :use_sudo, false
 
 set :user, 'volunteer2'
