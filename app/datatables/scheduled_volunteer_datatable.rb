68c5e3667faab8eafa3e56e4d92536bb034aa46f
Artem Gooscha <artem.gooscha@gmail.com>
2015-07-15 19:21:13 +0300

    Removed some fields from search on manage volunteers page

--- app/datatables/scheduled_volunteer_datatable.rb
+++ app/datatables/scheduled_volunteer_datatable.rb
@@ -10,9 +10,7 @@ class ScheduledVolunteerDatatable < AjaxDatatablesRails::Base
   end
 
   def searchable_columns
-    @searchable_columns ||= ['DepartmentBlock.start_time', 'DepartmentBlock.end_time', 
-                            'User.username', 'User.first_name', 'User.email',
-                            'Department.name', 'DepartmentBlock.name', 'Charity.name']
+    @searchable_columns ||= ['User.username', 'User.first_name', 'User.email', 'Charity.name']
   end
 
   private 
