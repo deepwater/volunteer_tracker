af69a38eb02ccada55a26243278b69f0d1be316a
Carl Smith <smith.carlton@gmail.com>
2019-02-15 17:32:18 -0500

    add puma to map bins

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :rbenv_ruby, '2.1.5'
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all # default value
 
 # Default value for :linked_files is []
